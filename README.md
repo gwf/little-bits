84b2988df76005315e75e8b8835ce23788df8e47
Gary William Flake <gary@flake.org>
2019-01-09 18:19:14 -0800

    playing with fib

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # little-bits :-)
 
-## Hello world!
+## fib.py
+
+Note:
+
+
+
+fib(4) = fib(3) + fib(2)
+       = fib(2) + fib(1) + fib(1) + fib(0)
+       = fib(1) + fib(0) + 1 + 1 + 0
+       = 1 + 0 + 1 + 1 + 0
+       = 3
+
+fib(3) = fib(2) + f(1)
+       = fib(1) + fib(0) + 1
+       = 1 + 0 + 1
+       = 2
--- fib.py
+++ fib.py
@@ -1,7 +1,22 @@
 
 
-def fib(x):
-  if (x <= 1): return x
-  else: return fib(x-1) + fib(x-2)
+def fib1(x):
+  if (x <= 1):
+    return x
+  else:
+    return fib(x-1) + fib(x-2)
 
-print(fib(10))
+def fib2(x):
+  f = [0] * (x+1)
+  for i in range(x+1):
+    if (i <= 1):
+      f[i] = i
+    else:
+      f[i] = f[i-1] + f[i-2]
+  return f[x]
+
+
+
+for x in range(100):
+  print(x, fib2(x))
+
